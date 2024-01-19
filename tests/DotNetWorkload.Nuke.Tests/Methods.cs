735193a4ef0d9853e7c11b927465bca8c6bb2c16
Enner Pérez <ennerperez@gmail.com>
2024-01-19 02:16:10 -0500

    General Update

---
+++ tests/DotNetWorkload.Nuke.Tests/Methods.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+namespace DotNetWorkload.Nuke.Tests
+{
+    internal static class Methods
+    {
+        internal static string GetSolutionDir()
+        {
+            return Path.Combine("..", "..", "..", "..", "..");
+        }
+    }
+}
--- tests/DotNetWorkload.Nuke.Tests/WorkloadTest.cs
+++ tests/DotNetWorkload.Nuke.Tests/WorkloadTest.cs
@@ -1,52 +1,54 @@
 using System.IO;
 using System.Linq;
 using Xunit;
-using static Nuke.Common.Tools.DotNet.Workload.WorkloadTasks;
+using Xunit.Extensions.Ordering;
+using static Nuke.Common.Tools.DotNet.Workload.Tasks;
 namespace DotNetWorkload.Nuke.Tests
 {
 
-    public class WorkloadTasks
+    [Order(1)]
+    public class WorkloadTest
     {
-        [Fact]
+        [Fact, Order(1)]
         public void Search()
         {
             var result = DotNetWorkloadSearch("a");
             Assert.True(result.Any());
         }
 
-        [Fact]
+        [Fact, Order(2)]
         public void SearchAsList()
         {
             var result = DotNetWorkloadSearchAsList("a");
             Assert.True(result.Any());
         }
 
-        [Fact]
+        [Fact, Order(3)]
         public void List()
         {
             var result = DotNetWorkloadList();
             Assert.True(result.Any());
         }
 
-        [Fact]
+        [Fact, Order(4)]
         public void ListAsList()
         {
             var result = DotNetWorkloadListAsList();
             Assert.True(result.Any());
         }
 
-        [Fact]
+        [Fact, Order(5)]
         public void RestoreSolution()
         {
-            var path = Path.GetFullPath(@"..\..\..\..\..\DotNetWorkload.Nuke.sln");
+            var path = Path.GetFullPath(Path.Combine(Methods.GetSolutionDir(), "DotNetWorkload.Nuke.sln"));
             var result = DotNetWorkloadRestore(path);
             Assert.Contains(result, m => m.Text.Contains("Successfully installed workload(s)"));
         }
 
-        [Fact]
+        [Fact, Order(6)]
         public void RestoreProject()
         {
-            var path = Path.GetFullPath(@"..\..\..\..\..\src\DotNetWorkload.Nuke\DotNetWorkload.Nuke.csproj");
+            var path = Path.GetFullPath(Path.Combine(Methods.GetSolutionDir(), "src", "DotNetWorkload.Nuke", "DotNetWorkload.Nuke.csproj"));
             var result = DotNetWorkloadRestore(path);
             Assert.Contains(result, m => m.Text.Contains("Successfully installed workload(s)"));
         }
